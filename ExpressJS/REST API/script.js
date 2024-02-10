175597783daa63d988cb4af36cb1d2052bae00dc
Ayush Sarodey <72145248+ayushsarode@users.noreply.github.com>
2024-02-10 16:49:43 +0530

    Update script.js

--- ExpressJS/REST API/script.js
+++ ExpressJS/REST API/script.js
@@ -1,14 +1,58 @@
-import express from "express";
+const express = require("express")
+const users = require("./MOCK_DATA.json")
 
 
 const app = express()
 const PORT = 3000;
 
 
-app.get('/', (req, res) => {
-    res.send("Hello world")
+app.get("/users", (req, res) => {
+    return res.json(users);
 })
 
+// app.get("/users", (req, res) => {
+//     const html = `<ul>
+//     ${users.map((user) => `<li>${user.first_name}</li>`
+//     </ul >`;
+//     res.send(html);
+// });
+
+
+// app.get("/users", (req, res) => {
+//     const html = `<ul>
+//     ${users.map((user) => `<li>${user.first_name}</li>`).join('')}
+//     </ul>`;
+//     res.send(html);
+// });
+
+app.get("/users/:id", (req, res) => {
+    try {
+        const id = Number(req.params.id);
+        const user = users.find((user) => user.id === id);
+        if (!user) {
+            throw new Error('User not found')
+        }
+
+        return res.json(user);
+    } catch (error) {
+        return res.status(404).json({ error: error.message });
+    }
+
+});
+
+app.post("/users", (req, res) => {
+    return res.json({ status: "pending" });
+})
+
+app.put("/users/:id", (req, res) => {
+    return res.json({ status: "pending" });
+})
+
+app.delete("/users/:id", (req, res) => {
+    return res.json({ status: "pending" });
+})
+
+
 app.listen(PORT, () => {
-    console.log("Server is running ${PORT}");
+    console.log(`Server is running ${PORT} `);
 })
