b0be32d712db57e9dfbb344594d0198380cc680e
Ayush Sarode <72145248+AyushSarode@users.noreply.github.com>
2024-05-04 14:09:08 +0530

    added

--- 4.URL-Shortener/index.js
+++ 4.URL-Shortener/index.js
@@ -1,4 +1,5 @@
-const express = require("express")
+const express = require("express");
+const path = require("path")
 const { connectToMongoDB } = require('./connection')
 const URL = require("./models/url")
 
@@ -11,25 +12,49 @@ connectToMongoDB('mongodb://localhost:27017/url-shortener').then(() => {
     console.log('MongoDB Successfully Connected');
 })
 
-
+//ejs 
+app.set('view engine', "ejs");
+app.set('views', path.resolve("./views"))
 
 app.use(express.json());
 
 app.use("/url", urlRoute);
 
+app.get("/test", async(req,res) => {
+    const allUrls  = await URL.find({});
+    return res.render('home',
+        {
+            urls:allUrls,
+            
+        },
+        
+    );
+});
+
 app.get("/:shortId", async (req, res) => {
-    const shortId = req.params.shortId;
-    const entry = await URL.findOneAndUpdate({
-        shortId
-    }, {
-        $push: {
-            visitHistory: {
-                timestamp: Date.now()
+    try {
+        const shortId = req.params.shortId;
+        const entry = await URL.findOneAndUpdate({
+            shortId
+        }, {
+            $push: {
+                visitHistory: {
+                    timestamp: Date.now()
+                }
             }
+        });
+
+        if (entry && entry.redirectURL) {
+            res.redirect(entry.redirectURL);
+        } else {
+            // Handle the case when entry is null or redirectURL is not available
+            res.status(404).send("URL Not Found");
         }
-    });
-    res.redirect(entry.redirectURL)
-})
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`${PORT} is listening...`);
