7e5c53cd4dad558ef4a3f3a56432ce399e7ea291
Ayush Sarode <72145248+AyushSarode@users.noreply.github.com>
2024-06-04 13:23:53 +0530

    multer

--- 6.Multer-FileUpload/index.js
+++ 6.Multer-FileUpload/index.js
@@ -1,20 +1,29 @@
 const PORT = 8001;
-const express = require('express')
+const express = require('express');
 const path = require('path');
-const multer = require("multer")
+const multer = require('multer');
+const { log } = require('console');
 
+const upload = multer({ dest: 'uploads/' });
+const app = express();
 
-const app = express()
-
-app.set("view engine", "ejs")
-app.set("views", path.resolve("./views"))
+app.set('view engine', 'ejs');
+app.set('views', path.resolve('./views'));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req,res) => {
-    return res.render("homepage")
-})
+app.get('/', (req, res) => {
+    return res.render('homepage');
+});
 
-app.listen(PORT, ()=> {
-console.log(`Server started at http://localhost:${PORT}`);
-})
+app.post('/upload', upload.single('profileImage'), (req, res) => {
+    console.log(req.body);
+    console.log(req.file);
+
+    return res.redirect('/');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}`);
+});
