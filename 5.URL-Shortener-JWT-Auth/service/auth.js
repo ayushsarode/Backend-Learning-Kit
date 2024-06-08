89ab7c3b2393fa11081e781274606f6fc0d1bd43
Ayush Sarode <72145248+AyushSarode@users.noreply.github.com>
2024-06-08 23:10:27 +0530

    project name changed

--- 5.URL-Shortener-JWT-Auth/service/auth.js
+++ 5.URL-Shortener-JWT-Auth/service/auth.js
@@ -3,8 +3,6 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const secret = process.env.SECRET_KEY;
 
-console.log(process.env);
-
 function setUser(user) {  
   try {
       return jwt.sign({
